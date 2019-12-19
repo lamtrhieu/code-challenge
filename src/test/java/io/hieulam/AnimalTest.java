c49bfa0ed3b44de4743115aea805880b9ad20b31
Hieu Lam <lamtrhieu@gmail.com>
2019-12-19 21:23:44 +0700

    Add Solution classe and implement sing method

---
+++ src/test/java/io/hieulam/AnimalTest.java
@@ -0,0 +1,19 @@
+package io.hieulam;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class AnimalTest {
+
+    @BeforeEach
+    void beforeEach() {
+    }
+
+
+    @AfterEach
+    void afterEach() {
+
+    }
+
+}
