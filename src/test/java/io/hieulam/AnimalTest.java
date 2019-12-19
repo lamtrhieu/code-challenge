5f806cea69fdaaf7c6f10bf54f8592a5e33542d2
Hieu Lam <lamtrhieu@gmail.com>
2019-12-19 21:30:46 +0700

    Add test for Animal walk method

--- src/test/java/io/hieulam/AnimalTest.java
+++ src/test/java/io/hieulam/AnimalTest.java
@@ -1,13 +1,37 @@
 package io.hieulam;
 
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+
+
+import java.io.PrintStream;
+
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class AnimalTest {
 
+
+    private static PrintStream printStream;
+    private static final PrintStream originalOut = System.out;
+
+    private Animal animal;
+
+    @BeforeAll
+    static void setup() {
+        printStream = mock(PrintStream.class);
+        System.setOut(printStream);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.setOut(originalOut);
+    }
+
     @BeforeEach
     void beforeEach() {
+        animal = new Animal();
     }
 
 
@@ -15,5 +39,12 @@ class AnimalTest {
     void afterEach() {
 
     }
+
+    @Test
+    public void testAnimalWalk() {
+        animal.walk();
+        verify(printStream).println("I am walking");
+    }
+
 
 }
